5d8a2e6cc42b966d8fda57d2abca3778ebb42444
Colin Tucker <colin@praxis.net.au>
2017-05-04 16:19:39 +1000

    Added _config.php file

---
+++ _config.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * SilverWare Datepicker configuration file.
+ *
+ * PHP version >=5.6.0
+ *
+ * For full copyright and license information, please view the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * @package SilverWare\Datepicker
+ * @author Colin Tucker <colin@praxis.net.au>
+ * @copyright 2017 Praxis Interactive
+ * @license https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @link https://github.com/praxisnetau/silverware-datepicker
+ */
+
+// Define Module Constants:
+
+if (!defined('SILVERWARE_DATEPICKER_DIR')) {
+    define('SILVERWARE_DATEPICKER_DIR', basename(__DIR__));
+}
+
+if (!defined('SILVERWARE_DATEPICKER_PATH')) {
+    define('SILVERWARE_DATEPICKER_PATH', realpath(__DIR__));
+}
